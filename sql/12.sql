4c657eb5275a38aee281ba1696be25c5673b03cd
GusAlbach <zepolalbach@gmail.com>
2024-02-18 16:00:25 -0800

    test

--- sql/12.sql
+++ sql/12.sql
@@ -1,6 +1,6 @@
 /*
  * Use a JOIN to list the total paid by each customer.
  * List the customers alphabetically by last name.
- * Use tables payment and customer.
+ * Use  tables payment and customer.
  */
 SELECT customer_id, first_name, last_name, SUM(amount) FROM payment JOIN customer USING (customer_id) GROUP BY first_name, last_name, customer_id ORDER BY last_name;
